7e9a254a501fdadbc37984df2674f78cdd81bf07
Nautilus1256 <nouch.000.0125@gmail.com>
2024-08-29 20:51:11 +0900

    バリデーションの実装

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -7,13 +7,15 @@
         </div>
         <div class="title">
             <h2>質問タイトル</h2>
-            <input type="text" name="question[title]" placeholder="タイトル"/>
+            <input type="text" name="question[title]" placeholder="タイトル" value="{{ old('question.title') }}"/>
+            <p class="title__error" style="color:red">{{ $errors->first('question.title') }}</p>
         </div>
         <div class="body">
             <h2>質問内容詳細</h2>
-            <textarea name="question[body]" placeholder="質問内容詳細"></textarea>
+            <textarea name="question[body]" placeholder="質問内容詳細">{{ old('question.body') }}</textarea>
+            <p class="body__error" style="color:red">{{ $errors->first('question.body') }}</p>
         </div>
-        <input type="submit" value="store"/>
+        <input type="submit" value="保存"/>
         <div class="footer">
             <a href="/" class="underline">戻る</a>
         </div>
