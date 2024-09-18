bf8dce48743b74d08539631b85b91d1289f82e38
Nautilus1256 <nouch.000.0125@gmail.com>
2024-09-18 20:51:02 +0900

    一覧表示画面のCSSの追加（１）

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,43 +1,107 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Index') }}
-        </h2>
-    </x-slot>
-    <h1>Tutor Connect</h1>
-    <div class='questions'>
-        @foreach ($questions as $question)
-            <div class='question'>
-                <h2 class='title'>
-                    <a href="/questions/{{ $question->id }}" class="underline">{{ $question->title }}</a>
-                </h2>
-                
-                @auth
-                    @if($question->isLikedByAuthUser())
-                        <div class="flexbox">
-                            <i class="fa-regular fa-heart like-btn liked" id={{$question->id}}></i>
-                            <p class="count-num">{{$question->question_likes->count()}}</p>
+    <div class='flex'>
+        <div class='w-3-10 border-r border-black'>
+            左30%
+        </div>
+        <div class='w-7-10'>
+            <div class='questions block mx-20'>
+                @foreach ($questions as $question)
+                    <div class='question flex flex-col py-4'>
+                        <div class='question-content flex justify-between border-2 rounded-2xl border-black p-4 h-20 max-w-full overflow-hidden box-content'>
+                            @if($question->images->isNotEmpty())
+                                <div class='question-letters-with-image'>
+                                    <h2 class='question-title'>
+                                        <a href='/questions/{{ $question->id }}' class='underline'>{{ $question->title }}</a>
+                                    </h2>
+                                    <p class='question-body truncate'>
+                                        {{ $question->body }}
+                                    </p>
+                                    <div class='question-category flex items-center justify-start'>
+                                        @if($question->categories->isNotEmpty())
+                                            <p>カテゴリー：</p>
+                                            @foreach ($question->categories as $category)
+                                                <p>{{ $category->name }}</p>
+                                            @endforeach
+                                        @else
+                                            <p>カテゴリー：なし</p>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class='question-image'>
+                                    <img src='{{ $question->images->first()->image_url }}' alt='画像' class='block box-border w-20 h-20 ml-5 border rounded-lg border-gray-400'>
+                                </div>
+                            @else
+                                <div class='question-letters-without-image'>
+                                    <h2 class='title'>
+                                        <a href='/questions/{{ $question->id }}' class='underline'>{{ $question->title }}</a>
+                                    </h2>
+                                    <p class='question-body truncate'>
+                                        {{ $question->body }}
+                                    </p>
+                                    <div class='question-category flex items-center justify-start'>
+                                        @if($question->categories->isNotEmpty())
+                                            <p>カテゴリー：</p>
+                                            @foreach ($question->categories as $category)
+                                                <p>{{ $category->name }}</p>
+                                            @endforeach
+                                        @else
+                                            <p>カテゴリー：なし</p>
+                                        @endif
+                                    </div>
+                                </div>
+                            @endif
                         </div>
-                    @else
-                        <div class="flexbox">
-                            <i class="fa-regular fa-heart like-btn" id={{$question->id}}></i>
-                            <p class="count-num">{{$question->question_likes->count()}}</p>
+                        <div class='question-info flex justify-end'>
+                            <div class='question-status flex items-center'>
+                                <p>{{ $question->status }}</p>
+                            </div>
+                            <div class='question-author flex items-center ml-4'>
+                                <i class='fa-regular fa-user'></i>
+                                <p class='ml-2'>{{ $question->user->name }}</p>
+                            </div>
+                            <div class='question-like ml-4 flex items-center'>
+                                @auth
+                                    @if($question->isLikedByAuthUser())
+                                        <i class='fa-regular fa-heart like-btn liked' id={{$question->id}}></i>
+                                        <p class='ml-2'>{{$question->question_likes->count()}}</p>
+                                    @else
+                                        <i class='fa-regular fa-heart like-btn' id={{$question->id}}></i>
+                                        <p class='ml-2'>{{$question->question_likes->count()}}</p>
+                                    @endif
+                                @endauth
+                                @guest
+                                    <a href='/login' class='block'>
+                                        <i class='fa-regular fa-heart'></i>
+                                    </a>
+                                    <p class='ml-2'>{{$question->question_likes->count()}}</p>
+                                @endguest
+                            </div>
+                            <div class='question-reply ml-4 flex items-center'>
+                                <i class="fa-regular fa-comment-dots"></i>
+                                <p class='ml-2'>{{$question->answers->count()}}</p>
+                            </div>
+                            <div class='question-create-date ml-4 flex items-center'>
+                                <i class="fa-solid fa-pen"></i>
+                                <p class='ml-2'>{{$question->created_at->format('Y/m/d')}}</p>
+                            </div>
                         </div>
-                    @endif
-                @endauth
-                
-                @guest
-                    <p>loginしていません</p>
-                @endguest
-                
+                        
+                        
+                        
+                        
+                    </div>
+                @endforeach
             </div>
-        @endforeach
+            <div class='paginate underline'>
+                {{ $questions->links() }}
+            </div>
+            <div class='post-btn'>
+                <a href='/questions/create'>
+                    <i class='fa-regular fa-pen-to-square post-icon'></i>
+                </a>
+            </div>
+        </div>
     </div>
-    <div>
-        <a href="/questions/create" class="underline">質問を新規作成</a>
-    </div>
-    <div class='paginate underline'>
-        {{ $questions->links() }}
-    </div>
+    
     
 </x-app-layout>
